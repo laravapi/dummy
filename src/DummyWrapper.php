f58c8443545dc42eb5671c97ead8e92aaa84871c
Sebastian Schöps <sebastian.schoeps@nanuc.io>
2022-01-13 13:17:57 +0100

    Initial commit

---
+++ src/DummyWrapper.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace LaravelApi\Dummy;
+
+class DummyWrapper
+{
+    public function config()
+    {
+        return [
+            'DUMMY_KEY_1' => [
+                'config' => 'services.dummy.key-1',
+                'description' => 'Description for config key 1',
+            ],
+            'DUMMY_KEY_2' => [
+                'config' => 'services.dummy.key-2',
+                'description' => 'Description for config key 2',
+            ],
+        ];
+    }
+
+    public function __call($name, $attributes)
+    {
+        dump('API Call to ' . $name . ' with attributes ' . $attributes);
+    }
+}
